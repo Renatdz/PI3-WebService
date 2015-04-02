697e5fdb340e1fa7e2549384e8a31f6e5e6d558d
Renato Mendes <renatodz86@gmail.com>
2015-04-01 21:01:21 -0300

    Nova classe implementada... Search(); para consumir o um site web, retornando um html para ser tratado em uma outra classe futura..
    Testes no main();

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
-import br.com.senac.ed.view.Login;
+import br.com.senac.ed.model.Search;
 
 
 public class Main {
 	public static void main(String[] args) {
-		Login tela = new Login();
 		
+		Search consumoWeb = new Search();
+		
+		System.out.println(consumoWeb.consumirSite("o+simbolo+perdido"));
+		//parametro busca ex: o+simbolo+perdido 
 		
 	}
-
 	
 	public Main() {
 		super();
---
+++ src/br/com/senac/ed/model/Search.java
@@ -0,0 +1,77 @@
+package br.com.senac.ed.model;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class Search {
+
+	private String retornoHTTP = null;
+	
+	public Search(){
+		//construtor vazio
+	}
+	
+	public String consumirSite(String stringLivro) {
+		//string web do site.
+		String urlString = "http://www.ciadoslivros.com.br/pesquisa?t="+stringLivro+"&f=&sr=GERAL";
+		
+		try {
+			URL url = new URL(urlString);
+			
+			HttpURLConnection connection = 
+					(HttpURLConnection) url.openConnection();
+			
+			connection.setRequestProperty("Request-Method", "GET");
+			connection.setDoInput(true);
+			connection.setDoOutput(false);
+			
+			connection.connect();
+			
+			BufferedReader br =  
+				    new BufferedReader(new InputStreamReader(connection.getInputStream())); 
+			
+			// le ate o final  
+			StringBuffer newData = new StringBuffer(10000);  
+			String s = "";  
+			while (null != ((s = br.readLine()))) {  
+			    newData.append(s);  
+			}  
+			br.close();
+			
+			PrintWriter out = new PrintWriter(System.out, true); 
+			
+			// imprime o numero do resultado  
+						out.println(  
+						    "Resultado: "  
+						        + connection.getResponseCode()  
+						        + "/"  
+						        + connection.getResponseMessage());
+			
+			// imprime o codigo resultante  
+			//out.println(new String(newData));  
+			
+			
+			this.retornoHTTP = new String(newData);
+						
+			return this.retornoHTTP;  
+			 
+			
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return this.retornoHTTP;
+		
+	}
+
+}
+
